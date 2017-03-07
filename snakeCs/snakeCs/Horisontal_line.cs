c4b92e2536e6c7b4a7fa72383df5563f7486b181
DimaSmile <efremov2293@gmail.com>
2017-03-07 19:20:41 +0200

    New2File

---
+++ snakeCs/snakeCs/Horisontal_line.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeCs
+{
+	public class Horisontal_line
+	{
+
+		List<Point> pList;
+
+		public Horisontal_line (int xLeft, int xReight, int y, char sym)
+		{
+			pList = new List<Point> ();
+			for (int x = xLeft; x <= xReight; x++) {
+				Point p = new Point (x, y, sym);
+				pList.Add (p);
+			}
+
+		}
+		public 	void Drow(){ //Метод Drow
+			foreach (Point p in pList) {
+				p.Draw (); 
+			}
+		}
+	}
+}
+
---
+++ snakeCs/snakeCs/Vertical_line.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeCs
+{
+	public class Vertical_line
+	{
+
+		List<Point> pList;
+
+		public Vertical_line (int x, int yLeft, int yReight, char sym)
+		{
+			pList = new List<Point> ();
+			pList = new List<Point> ();
+			for (int y = yLeft; y <= yReight; y++) {
+				Point p = new Point (x, y, sym);
+				pList.Add (p);
+			}
+
+		}
+		public 	void Drow(){ //Метод Drow
+			foreach (Point p in pList) {
+				p.Draw (); 
+			}
+
+		}
+	}
+}
+
